7e3e0722d0671cbe2f5384a166c427ec14f7018d
Adrian Sieber <mail@adriansieber.com>
2017-02-03 21:22:35 +0000

    Add boilerplate code for native swift app

---
+++ app/Perspectra/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Perspectra
+//
+//  Created by Adrian Sieber on 2017-01-27.
+//  Copyright © 2017 Feram. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
