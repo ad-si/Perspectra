e1dad00a3113853e72ad599c340ed01069914187
Adrian Sieber <mail@adriansieber.com>
2017-02-12 23:40:45 +0000

    Add modules to generate and remove noise

---
+++ core/perspectra/multipass_cleaner.py
@@ -0,0 +1,38 @@
+import numpy
+from skimage import morphology
+
+def remove_noise (original_img, passes, images):
+    """
+    Larger blobs must be increasingly separated to be labeled as noise
+    """
+    cleaned_orig = original_img
+    cleaned_eroded = original_img
+    radius_step = 2
+
+    for index in range(passes):
+        cummulative_dilation_size = radius_step ** index
+        cummulative_disk = morphology.disk(cummulative_dilation_size)
+        print(f'Cummulative dilation size: {cummulative_dilation_size}')
+
+        current_dilation_size = radius_step ** (index - 1) \
+            if index > 0 \
+            else 1
+        dilation_disk = morphology.disk(current_dilation_size)
+        print(f'Current dilation size: {current_dilation_size}')
+
+        # Noise blobs with up to 80 % more area
+        # than the structuring element will get deleted
+        max_noise_size = numpy.count_nonzero(cummulative_disk) * 1.8
+        print(f'Maximum noise size: {max_noise_size}')
+
+        eroded = morphology.dilation(
+            cleaned_eroded,
+            selem=morphology.disk(current_dilation_size)
+        )
+        images.append((f'eroded {index}', eroded))
+        cleaned_eroded = morphology.remove_small_objects(eroded, max_noise_size)
+        images.append((f'cleaned eroded {index}', cleaned_eroded))
+        cleaned_orig = numpy.logical_and(cleaned_orig, cleaned_eroded)
+        print(f'Finished cleaning pass {index}\n')
+
+    return cleaned_orig
---
+++ core/perspectra/noise_generator.py
@@ -0,0 +1,33 @@
+import random
+import numpy
+from skimage import morphology, util
+
+def add_noise (image):
+    base_radius = 2
+    base_disk = morphology.disk(base_radius)
+    total_amount = 0.001
+    empty_image = numpy.zeros_like(image)
+
+    noisy_img = util.random_noise(
+        image=empty_image,
+        mode='salt',
+        amount=total_amount,
+        seed=123,
+    )
+    closed_noise = morphology.binary_closing(
+        image=noisy_img,
+        selem=morphology.disk(15)
+    )
+    labeled_noise = morphology.label(closed_noise)
+    keep_percentage = 0.05
+    number_of_noise_blobs = len(numpy.unique(labeled_noise))
+    random.seed(a=123)
+    random_blob_labels = random.sample(
+        # Start with 1 to not get the background color, which is 0
+        range(1, number_of_noise_blobs),
+        int(number_of_noise_blobs * keep_percentage),
+    )
+    filtered_noise = numpy.in1d(labeled_noise.flat, random_blob_labels)
+    reshaped_noise = filtered_noise.reshape(image.shape)
+
+    return numpy.logical_or(reshaped_noise, image)
